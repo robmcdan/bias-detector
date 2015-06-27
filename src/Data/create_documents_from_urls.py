9bed81b0fb07cb694a56f16454b1886c2356f465
Rob McDaniel <robm@payscale.com>
2015-06-26 23:10:32 -0700

    initial checkin

---
+++ src/Data/create_documents_from_urls.py
@@ -0,0 +1,16 @@
+import goose
+import os
+
+
+def create(list_of_urls, path_for_docs):
+    """
+    creates a bunch of documents from a list of URLS
+    :param list_of_urls: list of URLS to crawl
+    :param path_for_docs: path to store docs, one per URL
+    :return:
+    """
+
+if __name__ == "__main__":
+    list = ["http://www.nytimes.com/2015/06/27/us/supreme-court-same-sex-marriage.html?_r=0",
+            "http://www.usatoday.com/story/news/nation/2015/06/25/guard-accused-delivering-tools-to-escapees/29263419/"]
+
---
+++ test/Data/test_create.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+__author__ = 'robm'
+
+class TestCreate(TestCase):
+  def test_create(self):
+    list = ["http://www.nytimes.com/2015/06/27/us/supreme-court-same-sex-marriage.html?_r=0",
+            "http://www.usatoday.com/story/news/nation/2015/06/25/guard-accused-delivering-tools-to-escapees/29263419/"]
+
+    self.fail()
